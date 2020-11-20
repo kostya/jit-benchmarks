90459e973248e4f2e2fea6496c5e119bff959473
Konstantin Makarchev <kostya27@gmail.com>
2020-11-20 09:06:41 +0300

    fix

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-find . -maxdepth 1 -type d \( ! -name \. \) -not -path "./tools" -not -path "./.git" -exec bash -c "cd '{}' && echo "RUN================""{}""===============" && ruby run.rb" \;
+find . -maxdepth 1 -type d \( ! -name \. \) -not -path "./tools" -not -path "./.git" -exec bash -c "cd '{}' && ruby run.rb" \;
